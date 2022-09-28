8195cc6e7e40de011d2460b309ff4e2fbcb63211
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-28 19:59:17 +1000

    Update streamlit_app.py
    Adding additional items to the menu page
    header, and 3 lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
+Streamlist.header('Breakfast Menu')
+Streamlist.text('Omega 3 & Blueberry Oatmeal')
+Streamlist.text('Kale, Spinach, & Rocket Smoothie')
+Streamlist.text('Hard-Boilded Free-Range Egg')
