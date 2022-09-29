f8a7aab64470f6bf2ab650dd0dd0a5fbead5e4e6
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 18:08:04 +1000

    Update streamlit_app.py
    changing the interface to interactive so the user can pick their fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display the fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
 streamlit.header('Fruityvice Fruit Advice!')
+
+#add instruction and picker for the user
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 #take the JSON version of the response and normalises it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
