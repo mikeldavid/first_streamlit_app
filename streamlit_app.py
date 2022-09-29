dfaab13d35f6da1f5e29682cda02a887ed66ac05
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 18:06:22 +1000

    Update streamlit_app.py
    Cleaning up the output removing JSON and changing the request api url

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display the fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "Kiwi")
 streamlit.header('Fruityvice Fruit Advice!')
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 #take the JSON version of the response and normalises it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
