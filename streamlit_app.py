99a9ca7f856ad699ad571c1ceb2bf58bed82efa4
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 21:54:36 +1000

    Update streamlit_app.py
    adding a nested if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,18 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display the fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+  
+  fruit_choice = streamlit.text_input('What fruit would you like information about?') #add instruction and picker for the user
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the JSON version of the response and normalises it
+    streamlit.dataframe(fruityvice_normalized) #output it to the screen as a table
 
-#add instruction and picker for the user
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-#take the JSON version of the response and normalises it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
+    except URLError as e:
+    streamlit.error()
 
 #do not run any code past this point 
 streamlit.stop()
