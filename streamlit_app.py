21cf36fcde4b7b9033af0690ee177a661e20b408
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 22:08:44 +1000

    Update streamlit_app.py
    formatting the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ def get_fruityvice_data(this_fruit_choice):
 #New Section to display the fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  
   fruit_choice = streamlit.text_input('What fruit would you like information about?') #add instruction and picker for the user
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
@@ -43,16 +42,24 @@ try:
 except URLError as e:
   streamlit.error()
 
-#do not run any code past this point 
-streamlit.stop()
-
-#Connecting to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+
+  
+  
+  #do not run any code past this point 
+streamlit.stop()
 
 #add instruction and picker for the user
 add_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
