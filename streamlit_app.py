6dc7bed9f3c78ee9b428bc4a48b62e1d2183ff63
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 21:50:04 +1000

    Update streamlit_app.py
    fixing typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 #do not run any code past this point 
-streamlist.stop()
+streamlit.stop()
 
 #Connecting to Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
