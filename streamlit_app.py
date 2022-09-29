12b3a329d4fcd5c3d7acb542742bd289c3d7ef4d
mikeldavid <47591105+mikeldavid@users.noreply.github.com>
2022-09-29 22:24:34 +1000

    Update streamlit_app.py
    fixing typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return streamlit.dataframe(fruityvice_normalized)
+  return fruityvice_normalized
 
 #New Section to display the fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
@@ -62,7 +62,7 @@ def insert_row_snowflake(new_fruit):
     return "Thank for adding " + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a Fruit Load List'):
+if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
